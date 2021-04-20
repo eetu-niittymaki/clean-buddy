4760d5df96b8c0bf8956422214a73c4151fd4f86
Eetu <eetu.niittymaki@tuni.fi>
2021-04-20 09:55:34 +0300

    Add new POST connections

--- database/connection.js
+++ database/connection.js
@@ -59,12 +59,15 @@ class ConnectionFunctions {
     })
   }
 
-  static saveCustomer (firstName, lastName, address, phone, email, password) {
+  // Add customer to DB
+  static saveCustomer (firstName, lastName, streetAddress, city, postcode, phone, email, password) {
     return new Promise((resolve, reject) => {
       if (connection) {
         const sql = `INSERT INTO customers (${connection.escape(firstName)},
                                             ${connection.escape(lastName)}, 
-                                            ${connection.escape(address)}, 
+                                            ${connection.escape(streetAddress)}, 
+                                            ${connection.escape(city)}, 
+                                            ${connection.escape(postcode)}, 
                                             ${connection.escape(phone)}, 
                                             ${connection.escape(email)}, 
                                             ${connection.escape(password)})`
@@ -76,11 +79,44 @@ class ConnectionFunctions {
       }
     })
   }
+
+  // Add supplier to DB
+  static saveSupplier (name, streetAddress, city, postcode, phone, email, password) {
+    return new Promise((resolve, reject) => {
+      if (connection) {
+        const sql = `INSERT INTO suppliers (${connection.escape(name)},
+                                            ${connection.escape(streetAddress)}, 
+                                            ${connection.escape(city)}, 
+                                            ${connection.escape(postcode)}, 
+                                            ${connection.escape(phone)}, 
+                                            ${connection.escape(email)}, 
+                                            ${connection.escape(password)})`
+        connection.query(sql, () => {
+          resolve('Added supplier to database ')
+        })
+      } else {
+        reject(Error)
+      }
+    })
+  }
+
+  // Add product to DB
+  static saveProduct (productName, productDescription, productPrice) {
+    return new Promise((resolve, reject) => {
+      if (connection) {
+        const sql = `INSERT INTO products (${connection.escape(productName)},
+                                            ${connection.escape(productDescription)}, 
+                                            ${connection.escape(productPrice)})`
+        connection.query(sql, () => {
+          resolve('Added product to database ')
+        })
+      } else {
+        reject(Error)
+      }
+    })
+  }
 }
 
 module.exports = ConnectionFunctions
 
 // curl -X POST 'first_name=Pekka&last_name=PEKKA&address=koti&phone=23124145&email=asda@dd.häh' http://localhost:8080/api
-
-// INSERT INTO products (supplier_id, product_name, product_description, product_price)
-// VALUES ((SELECT supplier_id from suppliers where name="Jaskan Firma"), "Ihan OK siivous", "Mä teen jotain", "25");
