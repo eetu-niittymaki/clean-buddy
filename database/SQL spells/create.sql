40d112022e5ab99c1d2ee61cc5439b34ef4eeb2b
Eetu <eetu.niittymaki@tuni.fi>
2021-04-20 09:52:00 +0300

    Add new tables and refractor old ones

--- database/SQL spells/create.sql
+++ database/SQL spells/create.sql
@@ -2,17 +2,22 @@ CREATE TABLE customers (
     customer_id INT AUTO_INCREMENT,
     first_name VARCHAR(45) NOT NULL,
     last_name VARCHAR(45) NOT NULL,
-    address VARCHAR(255) NOT NULL,
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    postcode VARCHAR(255) NOT NULL,
     phone VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(45),
     PRIMARY KEY(customer_id),
-    CONSTRAINT uc_address_phone_email_password UNIQUE(address, phone, email, password)
+    CONSTRAINT uc_address_phone_email_password UNIQUE(phone, email, password)
 );
 
 CREATE TABLE suppliers(
     supplier_id INT AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    postcode VARCHAR(255) NOT NULL,
     phone VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
@@ -25,7 +30,7 @@ CREATE TABLE products(
     supplier_id INT,
     product_name VARCHAR(45) UNIQUE,
     product_description VARCHAR(255),
-    product_price DECIMAL(15, 2),
+    product_price DECIMAL(15, 3),
     PRIMARY KEY (product_id),
     FOREIGN KEY (supplier_id)
         REFERENCES suppliers (supplier_id)
@@ -46,7 +51,7 @@ CREATE TABLE order_items(
     order_id INT,
     product_id INT,
     quantity INT NOT NULL,
-    total_price DECIMAL(15, 2),
+    total_price DECIMAL(15, 3),
     PRIMARY KEY(order_id, product_id),
     FOREIGN KEY (order_id)
         REFERENCES orders (order_id)
@@ -71,6 +76,20 @@ CREATE TABLE supplier_reviews(
         REFERENCES orders (order_id)
         ON UPDATE RESTRICT ON DELETE CASCADE
 );
+
+CREATE TABLE timed_offers(
+    offer_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
+    price DECIMAL(15, 3),
+    PRIMARY KEY(offer_id, product_id),
+    FOREIGN KEY (product_id)
+        REFERENCES products (product_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+INSERT INTO customers (first_name, last_name, street_address, city, postcode, phone, email, password)
+VALUES ("Pasi", "Virtanen", "Kotikatu 3 A", "Tampere", "33310", "+42312231", "email@email.com", "salsasana");
 
 --Get database table filesizes
 SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 AS "Size (MB)" FROM information_schema.TABLES GROUP BY table_schema
