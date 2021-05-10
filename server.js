1af55e2ac07c4350e14661f0bf76d2389ae74a9e
Toni Salminen <toni.salminen89@gmail.com>
2021-05-10 20:43:34 +0300

    fix typo

--- server.js
+++ server.js
@@ -264,7 +264,7 @@ router.put('/api/suppliers/', async (req, res) => {
   try {
     const supplierId = req.body.supplier_id
     const name = req.body.name
-    const supplierDescription = req.body.supplier_ddescription
+    const supplierDescription = req.body.supplier_description
     const streetAddress = req.body.street_address
     const city = req.body.city
     const postcode = req.body.postcode
